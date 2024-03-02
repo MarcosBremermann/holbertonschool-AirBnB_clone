5d1bf0c7acc3525dc274aad9229d3907ab15a7c0
IsmaelBuere <7674@holbertonstudents.com>
2024-03-02 16:06:13 +0000

    Storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,8 +5,12 @@ This is a file for the class FileStorage
 import json
 from os import path
 from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.user import User
-
 
 class FileStorage:
     """
