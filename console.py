564f37234412230f2ee2af797fb517bd652a0930
IsmaelBuere <7674@holbertonstudents.com>
2024-03-02 16:06:48 +0000

    Console

--- console.py
+++ console.py
@@ -3,8 +3,13 @@
 This module contains the Console and its methods
 """
 import cmd
-from models.base_model import BaseModel
 from models import storage
+from models.base_model import BaseModel
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 from models.user import User
 
 
