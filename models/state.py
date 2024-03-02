f57f32ec91048f5ebf368013ac2e4b0ed3312c56
IsmaelBuere <7674@holbertonstudents.com>
2024-03-02 16:01:45 +0000

    State

---
+++ models/state.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines State as a class.
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """ State class that inherits from BaseModel """
+    name = ""
